b4ad160689aeb459f5dff5929604a76c72fb9a8a
Prerak Bisen <149325207+prerakbisen@users.noreply.github.com>
2026-01-06 04:35:33 +0530

    Update CORS allowed origin for security configuration

--- Hgs/src/main/java/com/Info_intern/Hgs/config/SecurityConfig.java
+++ Hgs/src/main/java/com/Info_intern/Hgs/config/SecurityConfig.java
@@ -38,7 +38,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("https://prerakbisen.github.io"));
+        config.setAllowedOrigins(List.of("https://prerakbisen.github.io/hgs"));
         config.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
