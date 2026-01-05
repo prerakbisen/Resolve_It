5ae24cbba7a28b6bed45ad3d93d5bbc43eb08734
Prerak Bisen <149325207+prerakbisen@users.noreply.github.com>
2026-01-06 05:14:27 +0530

    Create GrievanceImageRepository.java

---
+++ Hgs/src/main/java/com/Info_intern/Hgs/repository/GrievanceImageRepository.java
@@ -0,0 +1,7 @@
+package com.Info_intern.Hgs.repository;
+
+import com.Info_intern.Hgs.model.GrievanceImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GrievanceImageRepository extends JpaRepository<GrievanceImage, Long> {
+}
